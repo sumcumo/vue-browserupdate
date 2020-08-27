f6ec55b1966c9d7e404df3226875380ed0b32c03
Marcel Jahn <marcel.jahn@sumcumo.com>
2020-08-27 20:02:30 +0200

    Revert "Merge pull request #2 from sumcumo/chore/updates"
    This reverts commit 40c1ac73548b132890b2482930a7a130eab6cd4b, reversing
    changes made to 517ee5e4dde2b326e703138419c461c7e7594e50.

--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,78 @@
-import browserUpdate from 'browser-update';
+'use strict';
 
-function addLoadEvent(func) {
-  const oldOnLoad = window.onload;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _browserUpdate = require('browser-update');
+
+var _browserUpdate2 = _interopRequireDefault(_browserUpdate);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function addLoadEvent(func) {
+  var oldOnLoad = window.onload;
   if (typeof window.onload !== 'function') {
     window.onload = func;
   } else {
-    window.onload = () => {
+    window.onload = function () {
       if (oldOnLoad) {
         oldOnLoad();
       }
       func();
-    }
+    };
   }
 }
 
-const VueBrowserUpdate = {};
-VueBrowserUpdate.install = (Vue, opts) => {
-  const options = opts;
-  const funcs = {
+var VueBrowserUpdate = {};
+VueBrowserUpdate.install = function (Vue, opts) {
+  var options = opts;
+  var funcs = {
     show: [],
     click: [],
-    close: [],
+    close: []
   };
   function dispatch(func, infos) {
-    funcs[func].forEach(f => f(infos));
+    funcs[func].forEach(function (f) {
+      return f(infos);
+    });
   }
 
-  addLoadEvent(() => {
-    options.options.onshow = infos => dispatch('show', infos);
-    options.options.onclick = infos => dispatch('click', infos);
-    options.options.onclose = infos => dispatch('close', infos);
+  addLoadEvent(function () {
+    options.options.onshow = function (infos) {
+      return dispatch('show', infos);
+    };
+    options.options.onclick = function (infos) {
+      return dispatch('click', infos);
+    };
+    options.options.onclose = function (infos) {
+      return dispatch('close', infos);
+    };
 
-    if (typeof browserUpdate === 'undefined') {
+    if (typeof _browserUpdate2.default === 'undefined') {
       throw new Error('The plugin "browser-update" could not be loaded.');
     } else {
       if (!options.containerAsync) {
-        browserUpdate(options.options, options.test);
+        (0, _browserUpdate2.default)(options.options, options.test);
       }
     }
   });
 
   Vue.browserUpdate = {
-    onshow: func => funcs.show.push(func),
-    onclick: func => funcs.click.push(func),
-    onclose: func => funcs.close.push(func),
-    appendContainer: (container) => {
-      if (container) options.options.container = container;
-      browserUpdate(options.options, options.test);
+    onshow: function onshow(func) {
+      return funcs.show.push(func);
     },
+    onclick: function onclick(func) {
+      return funcs.click.push(func);
+    },
+    onclose: function onclose(func) {
+      return funcs.close.push(func);
+    },
+    appendContainer: function appendContainer(container) {
+      if (container) options.options.container = container;
+      (0, _browserUpdate2.default)(options.options, options.test);
+    }
   };
 };
 
-export default VueBrowserUpdate;
+exports.default = VueBrowserUpdate;
--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import gulp from 'gulp'
-
-gulp.task('default', () => {
-  return gulp.src('index.js')
-  .pipe(gulp.dest('dist'))
-})
